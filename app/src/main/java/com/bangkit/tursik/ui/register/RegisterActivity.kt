f07a9382a347117a2736d789a7cb944f9e23bff0
farhanzqn <farhanzidan5@gmail.com>
2023-06-05 14:35:39 +0700

    membuat layout login dan register

---
+++ app/src/main/java/com/bangkit/tursik/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.tursik.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.tursik.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
